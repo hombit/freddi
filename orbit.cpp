1c0cccb3b7ea567854cda5c60c18e7b65712cc25
Konstantin Malanchev <hombit@gmail.com>
2016-07-05 11:59:01 +0300

    Add effeciency_of_accretion function

--- orbit.cpp
+++ orbit.cpp
@@ -8,8 +8,16 @@ double r_out_func(const double Mx, const double Mopt, const double P){
 	return 0.8 * roche;
 }
 
-double r_in_func(double Mx, double A){ // From «Black Hole Accretion Disks», A.44 (p. 530)
-			double Z1 = 1. + cbrt( (1.-A*A) ) * ( cbrt( (1.+A) ) + cbrt( (1.-A) ) );
-			double Z2 = sqrt( 3.*A*A + Z1*Z1 );
-			return GSL_CONST_CGSM_GRAVITATIONAL_CONSTANT * Mx / (GSL_CONST_CGSM_SPEED_OF_LIGHT * GSL_CONST_CGSM_SPEED_OF_LIGHT) * ( 3. + Z2 - sqrt( (3.-Z1) * (3.+Z1+2.*Z2) ) );
+double r_ISCO(const double kerr){ // From «Black Hole Accretion Disks», A.44 (p. 530)
+    double Z1 = 1. + cbrt( (1.-kerr*kerr) ) * ( cbrt( (1.+kerr) ) + cbrt( (1.-kerr) ) );
+	double Z2 = sqrt( 3.*kerr*kerr + Z1*Z1 );
+    return 3. + Z2 - sqrt( (3.-Z1) * (3.+Z1+2.*Z2) );
+}
+
+double r_in_func(const double Mx, const double kerr){
+	return GSL_CONST_CGSM_GRAVITATIONAL_CONSTANT * Mx / (GSL_CONST_CGSM_SPEED_OF_LIGHT * GSL_CONST_CGSM_SPEED_OF_LIGHT) * r_ISCO(kerr);
 };
+
+double efficiency_of_accretion(const double kerr){
+    return 1. - sqrt(1. - 2./3. / r_ISCO(kerr));
+}
--- orbit.hpp
+++ orbit.hpp
@@ -8,8 +8,11 @@
 
 double r_out_func(const double Mx, const double Mopt, const double P);
 
-double r_in_func(double Mx, double A);
+double r_ISCO(const double kerr);
 
+double r_in_func(double Mx, double kerr);
+
+double efficiency_of_accretion(const double kerr);
 
 
 #endif // _ORBIT_HPP
