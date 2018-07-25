0abc78a8397c54d6b319afb7461eb4198cb06790
Konstantin Malanchev <hombit@gmail.com>
2018-07-25 15:35:07 +0300

    Add tests of lambdas

--- test/regression.py
+++ test/regression.py
@@ -2,13 +2,13 @@ import glob
 import os
 import re
 import unittest
-from configparser import ConfigParser
+from collections import UserDict
+from configparser import ConfigParser, RawConfigParser
 
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal
 
 from freddi import Arguments, Freddi
-
 
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
@@ -18,25 +18,47 @@ class RegressionTestCase(unittest.TestCase):
     def setUp(self):
         self.data_files = glob.glob(os.path.join(DATA_DIR, '*.dat'))
 
-    @staticmethod
-    def load_config(data_file, arguments_to_remove=()):
-        section = 'default'
+    class MultiDict(UserDict):
+        def __setitem__(self, key, value):
+            if isinstance(value, list):
+                value = value[0]
+            if key not in self.data:
+                self.data[key] = value
+                return
+            if value == self.data[key]:
+                return
+            if isinstance(self.data[key], tuple):
+                if value in self.data[key]:
+                    return
+                self.data[key] += (value,)
+                return
+            self.data[key] = (self.data[key], value)
+
+    def convert_config_value(self, value):
+        if isinstance(value, tuple):
+            return [self.convert_config_value(v) for v in value]
+        try:
+            return float(value)
+        except ValueError:
+            return value.encode()
+
+    def load_config(self, data_file, arguments_to_remove=()):
+        section = 'section'
         section_str = '[{}]\n'.format(section)
         regexp = re.compile(r'^# ([^-])')
         with open(data_file) as f:
             lines = (regexp.sub(r'\1', line) for line in f if regexp.match(line))
             config_str = section_str + ''.join(lines)
-        config = ConfigParser(default_section=None)
+        config = RawConfigParser(strict=False, dict_type=self.MultiDict, inline_comment_prefixes=('#', ';'))
         config.optionxform = str
         config.read_string(config_str)
-        config = dict(config[section])
+        config = dict(config[section].items())
         for kwarg in arguments_to_remove:
             config.pop(kwarg, None)
         for name, value in config.items():
-            try:
-                config[name] = float(value)
-            except ValueError:
-                config[name] = value.encode()
+            config[name] = self.convert_config_value(value)
+        if 'lambda' in config:
+            config['lambdas'] = config.pop('lambda')
         config['cgs'] = False
         return config
 
@@ -52,4 +74,11 @@ class RegressionTestCase(unittest.TestCase):
             assert_equal(result.t[:, 0], data['t'] * 86400)
             for column in columns_to_compare:
                 assert_allclose(getattr(result, column)[:, 0], data[column], rtol=1e-5,
-                                err_msg='Column {}:'.format(column))
+                                err_msg='File: {}, column {}:'.format(data_file, column))
+            for i_lmbd, lmbd in enumerate(args.lambdas):
+                column = 'Fnu{}'.format(i_lmbd)
+                if column in data.dtype.names:
+                    assert_allclose(result.flux(lmbd)[:, 0], data[column], rtol=1e-5,
+                                    err_msg='File: {}, lambda {}, column {}:'.format(data_file, lmbd, column))
+                else:
+                    break
