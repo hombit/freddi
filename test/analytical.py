e5b728d88b7f646a2c1e3eb6d28d3b99c83ac052
Konstantin Malanchev <hombit@gmail.com>
2018-08-07 20:16:43 +0300

    Add test for Lipunova-Shakura OPAL solution

--- test/analytical.py
+++ test/analytical.py
@@ -8,8 +8,8 @@ from freddi import Freddi
 DAY = 86400
 
 
-class StationaryDiskTestCase(unittest.TestCase):
-    def test_shakura_sunayev_subcritical(self):
+class ShakuraSunyaevSubctriticalTestCase(unittest.TestCase):
+    def test(self):
         Mdot = 1e18
         fr = Freddi(initialcond=b'sinusF', Mdot0=Mdot/2, Mdotout=Mdot, time=1000*DAY, tau=1*DAY)
         for state in fr:
@@ -17,18 +17,29 @@ class StationaryDiskTestCase(unittest.TestCase):
         h = state.h
         assert_allclose(state.F, Mdot * (h - h[0]))
 
-    def test_lipunova_shalura(self):
-        fr = Freddi(opacity=b'Kramers', time=1000*DAY, tau=1*DAY)
+
+class LipunovaShakuraTestCase(unittest.TestCase):
+    def F_LS(self, h, a, kl):
+        xi = h / h[-1]
+        f = a[0] * xi + a[1] * xi**kl[0] + a[2] * xi**kl[1]
+        return f * (h - h[0]) / (h[-1] - h[0]) / xi
+
+    def check_freddi(self, opacity, a, kl):
+        fr = Freddi(opacity=opacity.encode(), time=1000*DAY, tau=1*DAY)
         for state in fr:
             pass
         h = state.h
         F = state.F
-        xi = h / h[-1]
-        a0 = 1.430
-        a1 = -0.460
-        a2 = 0.030
-        k = 3.5
-        l = 6.0
-        f = a0 * xi + a1 * xi**k + a2 * xi**l
-        F_LS = F[-1] * f * (h - h[0]) / (h[-1] - h[0]) * h[-1] / h
-        assert_allclose(F[-10:], F_LS[-10:], rtol=1e-3)
+        assert_allclose(F / F[-1], self.F_LS(h, a, kl), rtol=1e-3)
+
+    def test_kramers(self):
+        opacity = 'Kramers'
+        a = (1.430, -0.460, 0.030)
+        kl = (3.5, 6.0)
+        self.check_freddi(opacity, a, kl)
+
+    def test_opal(self):
+        opacity = 'OPAL'
+        a = (1.400, -0.425, 0.025)
+        kl = (11. / 3., 19. / 3.)
+        self.check_freddi(opacity, a, kl)
