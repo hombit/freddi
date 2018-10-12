a2225112d0527bbd97453d4d81c292c9b099c5fd
Konstantin Malanchev <hombit@gmail.com>
2018-10-12 15:17:54 +0300

    Add docstrings to tests

--- test/analytical.py
+++ test/analytical.py
@@ -16,6 +16,14 @@ DAY = 86400
 
 class ShakuraSunyaevSubctriticalTestCase(unittest.TestCase):
     def test(self):
+        """Shakura-Sunyaev 1973 stationary subcritical disk
+
+        The analytical stationary solution is $F ~ (h - h_{in})$
+
+        The initial condition is non-stationary and does not satisfies outer
+        boundary condition.
+
+        """
         Mdot = 1e18
         fr = Freddi(initialcond=b'sinusF', Mdot0=Mdot/2, Mdotout=Mdot, time=1000*DAY, tau=1*DAY)
         for state in fr:
@@ -26,6 +34,20 @@ class ShakuraSunyaevSubctriticalTestCase(unittest.TestCase):
 
 class ShakuraSunyaevSupercriticalTestCase(unittest.TestCase):
     def test(self):
+        """Shakura-Sunayev 1973 stationary supercritical disk with outflow
+
+        $d\dot M_w / dA = -\dot M_{crit} / (2 \pi R_{sph} r)$
+
+        The analytical stationary solution is $F ~ (h^3 - h^3_{in})$ for
+        the region where disk is supercritical: radius $r$ is between $R_{in}$
+        and $R_{sph} = R_{in} \dot M_{out} / \dot M_{crit}$, $Mdot_{crit}$ is
+        critical (Eddington) accretion rate on the black hole.
+
+        The initial condition is non-stationary and does not satisfies outer
+        boundary condition, outer radius is $R_{sph}$ for given accretion rate
+        through this radius.
+
+        """
         Mx = 2e34
         GM = 6.673e-8 * Mx
         c = 2.99792458e10
@@ -48,6 +70,20 @@ class StationaryWindATestCase(unittest.TestCase):
 
     @unittest.skipIf(scipy is None, 'No scipy module is available')
     def test(self):
+        """Stationary disk with outflow rate proportional to accretion rate
+
+        Outflow rate $d\dot M / dA ~ dF/dh$:
+
+        $d\dot M_w / dA = -k_A (h - h_{in}) / (h_{out} - h_{in})^2
+            \times 4\pi h^3 / (GM)^2 \times dF/dh$
+
+        Stationary solution is
+        $F ~ erfi(\sqrt{k_A/2} (h - h_{in})/(h_{out} - h_{in}))$
+
+        The initial condition is non-stationary and does not satisfies outer
+        boundary condition.
+
+        """
         Mdot = 1e18
         fr = Freddi(wind=b'__testA__', Mdotout=Mdot, initialcond=b'sinusF', Mdot0=Mdot,
             time=1000*DAY, tau=1*DAY, Nx=10000, gridscale=b'linear')
@@ -63,6 +99,20 @@ class StationaryWindBTestCase(unittest.TestCase):
     _k_B0 = 16.0
 
     def test(self):
+        """Stationary disk with outflow rate proportional to viscous torque
+
+        Outflow rate $d\dot M / dA ~ F$:
+
+        $d\dot M_w / dA = -k_B / (h_{out} - h_{in})^2 \times 4\pi h^3 / (GM)^2
+            \times F$
+
+        Stationary solution is
+        $F ~ sinh(\sqrt{k_B} (h - h_{in})/(h_{out} - h_{in}))$
+
+        The initial condition is non-stationary and does not satisfies outer
+        boundary condition.
+
+        """
         Mdot = 1e18
         fr = Freddi(wind=b'__testB__', Mdotout=Mdot, initialcond=b'sinusF', Mdot0=Mdot,
                     time=1000*DAY, tau=1*DAY, Nx=10000, gridscale=b'linear')
@@ -80,6 +130,17 @@ class StationaryWindCTestCase(unittest.TestCase):
     _k_C0 = 3.0
 
     def test(self):
+        """Stationary disk with constant outflow rate
+
+        Outflow rate $d\dot M / dA$ doesn't depend on F nor dF/dh
+
+        $d\dot M_w / dA = k_C / 2 (\cos(2\pi (h - h_w)/(h_{out} - h_w)) - 1)$
+        for $h > h_w$ and zero otherwise
+
+        The initial condition is non-stationary and does not satisfies outer
+        boundary condition.
+
+        """
         Mdot = 1e18
         fr = Freddi(wind=b'__testC__', Mdotout=Mdot, initialcond=b'sinusF', Mdot0=Mdot,
                     time=10000*DAY, tau=10*DAY, gridscale=b'linear')
@@ -98,7 +159,18 @@ class StationaryWindCTestCase(unittest.TestCase):
 
 
 class LipunovaShakuraTestCase(unittest.TestCase):
-    def F_LS(self, h, a, kl):
+    """Test Lipunova-Shakura 2000 quasi-stationary solutions
+
+    The analytical stationary solution is $F ~ (h - h_{in}) / h * P(h)$, where
+    $P(h)$ is trinomial.
+
+    The initial condition is non-stationary and does not satisfies outer
+    boundary condition.
+
+    """
+
+    @staticmethod
+    def F_LS(h, a, kl):
         xi = h / h[-1]
         f = a[0] * xi + a[1] * xi**kl[0] + a[2] * xi**kl[1]
         return f * (h - h[0]) / (h[-1] - h[0]) / xi
@@ -112,12 +184,14 @@ class LipunovaShakuraTestCase(unittest.TestCase):
         assert_allclose(F / F[-1], self.F_LS(h, a, kl), rtol=1e-3)
 
     def test_kramers(self):
+        """Lipunova-Shakura 2000 quasi-stationary solution for Kramers opacity"""
         opacity = 'Kramers'
         a = (1.430, -0.460, 0.030)
         kl = (3.5, 6.0)
         self.check_freddi(opacity, a, kl)
 
     def test_opal(self):
+        """Lipunova-Shakura 2000 quasi-stationary solution for "Opal" opacity"""
         opacity = 'OPAL'
         a = (1.400, -0.425, 0.025)
         kl = (11. / 3., 19. / 3.)
