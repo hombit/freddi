b9f0c7733e3c6ab7fbe9a5b7edcf2a20f12566b8
Konstantin Malanchev <hombit@gmail.com>
2019-03-08 18:59:46 +0300

    Introduce Simpson quadrature

--- cpp/src/util.cpp
+++ cpp/src/util.cpp
@@ -2,6 +2,10 @@
 
 #include <cmath>
 
+#include <boost/math/special_functions/pow.hpp>
+
+namespace bm = boost::math;
+
 double trapz(const vecd& x, const vecd& y, const size_t first, const size_t last) {
 	if (first >= last) {
 		return 0.;
@@ -14,7 +18,7 @@ double trapz(const vecd& x, const vecd& y, const size_t first, const size_t last
 }
 
 
-double trapz(const vecd& x, std::function<double (size_t)> f, const size_t first, const size_t last) {
+double trapz(const vecd& x, const std::function<double (size_t)>& f, const size_t first, const size_t last) {
 	if (first >= last) {
 		return 0.;
 	}
@@ -31,6 +35,64 @@ double disk_radial_trapz(const vecd& r, const vecd& y, const size_t first, const
 }
 
 
-double disk_radial_trapz(const vecd& r, std::function<double (size_t)> f, const size_t first, const size_t last) {
+double disk_radial_trapz(const vecd& r, const std::function<double (size_t)>& f, const size_t first, const size_t last) {
 	return trapz(r, [&r, f](const size_t i) -> double { return 2*M_PI * r[i] * f(i); }, first, last);
+}
+
+
+double simps(const vecd& x, const vecd& y, const size_t first, const size_t last) {
+	const size_t N = last - first + 1;
+	switch (N) {
+		case 0:
+			return 0;
+		case 1:
+			return 0;
+		case 2:
+			return 0.5 * (y[first] + y[last]) * (x[last] - x[first]);
+	}
+	if (N % static_cast<size_t>(2) == static_cast<size_t>(0)) {
+		return simps(x, y, first + 1, last) + simps(x, y, first, first + 1);
+	}
+
+	double delta = (x[first+2] - x[first]);
+	double s = y[first] * delta * (2. - (x[first+2] - x[first+1]) / (x[first+1] - x[first]))
+			+ y[first+1] * bm::pow<3>(delta) / ((x[first+2] - x[first+1]) * (x[first+1] - x[first]))
+			+ y[last] * (2. - (x[last-1] - x[last]) / (x[last] - x[last] - 1));
+	for (size_t i = first + 2; i <= last - 2; i += 2) {
+		delta = x[i+2] - x[i];
+		s += y[i] * (delta * (2. - (x[i+2] - x[i+1]) / (x[i+1] - x[i]))
+				+ (x[i] - x[i-2]) * (2. - (x[i-1] - x[i-2]) / (x[i] - x[i-1])))
+			+ y[i+1] * bm::pow<3>(delta) / ((x[i+2] - x[i+1]) * (x[i+1] - x[i]));
+	}
+	s /= 6.;
+	return s;
+}
+
+
+double simps(const vecd& x, const std::function<double (size_t)>& f, const size_t first, const size_t last) {
+	const size_t N = last - first + 1;
+	switch (N) {
+		case 0:
+			return 0;
+		case 1:
+			return 0;
+		case 2:
+			return 0.5 * (f(first) + f(last)) * (x[last] - x[first]);
+	}
+	if (N % static_cast<size_t>(2) == static_cast<size_t>(0)) {
+		return simps(x, f, first + 1, last) + simps(x, f, first, first + 1);
+	}
+
+	double delta = (x[first+2] - x[first]);
+	double s = f(first) * delta * (2. - (x[first+2] - x[first+1]) / (x[first+1] - x[first]))
+			   + f(first+1) * bm::pow<3>(delta) / ((x[first+2] - x[first+1]) * (x[first+1] - x[first]))
+			   + f(last) * (2. - (x[last-1] - x[last]) / (x[last] - x[last] - 1));
+	for (size_t i = first + 2; i <= last - 2; i += 2) {
+		delta = x[i+2] - x[i];
+		s += f(i) * (delta * (2. - (x[i+2] - x[i+1]) / (x[i+1] - x[i]))
+					 + (x[i] - x[i-2]) * (2. - (x[i-1] - x[i-2]) / (x[i] - x[i-1])))
+			 + f(i+1) * bm::pow<3>(delta) / ((x[i+2] - x[i+1]) * (x[i+1] - x[i]));
+	}
+	s /= 6.;
+	return s;
 }
