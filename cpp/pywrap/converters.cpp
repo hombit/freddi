a92b0b9c20df858006fa8ff6b9ac6566fe1a1214
Konstantin Malanchev <hombit@gmail.com>
2019-04-15 15:49:14 +0300

    Dict-map conversion

--- cpp/pywrap/converters.cpp
+++ cpp/pywrap/converters.cpp
@@ -1,3 +1,7 @@
+#include <map>
+#include <string>
+#include <vector>
+
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
 #include <numpy/arrayobject.h>
@@ -60,7 +64,60 @@ struct NumpyToVectorConverter {
 };
 
 
+template<typename Key, typename T>
+struct MapToDictConverter {
+	static PyObject* convert(const std::map<Key, T> m) {
+		dict d;
+		for (const auto &x: m) {
+			d[x.first] = x.second;
+		}
+		return incref(d.ptr());
+	}
+};
+
+
+template<typename Key, typename T>
+struct DictToMapConverter {
+	DictToMapConverter() {
+		converter::registry::push_back(convertible, construct, type_id<std::map<Key, T> >());
+	}
+	static void* convertible(PyObject* obj) {
+		if (!PyMapping_Check(obj)) {
+			return nullptr;
+		}
+		return obj;
+	}
+	static void construct(PyObject* obj, converter::rvalue_from_python_stage1_data* data) {
+		handle<> handle(obj);
+		dict d(handle);
+		std::map<Key, T> m;
+
+		const stl_input_iterator<object> begin(d), end;
+		for (auto key = begin; key != end; ++key) {
+			extract<Key> get_key(*key);
+			if (!get_key.check()) {
+				PyErr_SetString(PyExc_TypeError, "Wrong mapping key type");
+				throw error_already_set();
+			}
+			extract<T> get_value(d[*key]);
+			if (!get_value.check()) {
+				PyErr_SetString(PyExc_TypeError, "Wrong mapping value type");
+				throw error_already_set();
+			}
+			m[get_key()] = get_value();
+		}
+
+		auto storage = reinterpret_cast<converter::rvalue_from_python_storage<std::map<Key, T> >*>(data)->storage.bytes;
+		new(storage) std::map<Key, T>(std::move(m));
+		data->convertible = storage;
+	}
+};
+
+
 void register_converters() {
 	to_python_converter<vecd, VectorToNumpyConverter<double>, false>();
 	NumpyToVectorConverter<double, NPY_DOUBLE>();
+
+	to_python_converter<std::map<std::string, double>, MapToDictConverter<std::string, double>, false>();
+	DictToMapConverter<std::string, double>();
 }
