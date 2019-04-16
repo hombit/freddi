5e2e4eb893ab0acfd6c59dc2a6021d015cfe8318
Konstantin Malanchev <hombit@gmail.com>
2019-04-16 18:10:13 +0300

    Try to fix boost installation

--- .ci/install-boost.sh
+++ .ci/install-boost.sh
@@ -8,6 +8,6 @@ BOOST_DOT_VERSION="1.${1}.0"
 curl -LOJ https://dl.bintray.com/boostorg/release/${BOOST_DOT_VERSION}/source/boost_${BOOST_VERSION}.tar.gz
 tar --gzip -xf boost_${BOOST_VERSION}.tar.gz
 cd boost_${BOOST_VERSION}
-./bootstrap.sh --with-libraries=program_options,python --with-python=python3
+./bootstrap.sh --with-libraries=program_options,python --with-python=python3,numpy
 ./b2 link=shared,static
 ./b2 install
