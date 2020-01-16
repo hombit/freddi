f6d7bd5f9588a1f8971eae55017084e3b8cf1b25
Konstantin Malanchev <hombit@gmail.com>
2020-01-16 10:45:47 +0300

    freddi-ns outputs Thotspot column

--- cpp/src/output.cpp
+++ cpp/src/output.cpp
@@ -210,9 +210,10 @@ std::vector<FileOutputShortField> FreddiNeutronStarFileOutput::initializeShortFi
 	fields.emplace_back("Rin", "cm", [freddi]() {return freddi->R()[freddi->first()];});
 	fields.emplace_back("Lxns", "erg/s", [freddi]() {return freddi->Lx_ns();});
 	fields.emplace_back("Lbolns", "erg/s", [freddi]() {return freddi->Lbol_ns();});
+	fields.emplace_back("Thotspot", "keV", [freddi]() {return kToKev(freddi->T_hot_spot());});
 	fields.emplace_back("fpin", "float", [freddi]() {return freddi->fp();});
 	fields.emplace_back("Fmagnin", "dyn*cm", [freddi]() {return freddi->Fmagn()[freddi->first()];});
-        fields.emplace_back("Fin", "dyn*cm", [freddi]() {return freddi->F()[freddi->first()];});
+	fields.emplace_back("Fin", "dyn*cm", [freddi]() {return freddi->F()[freddi->first()];});
 	return fields;
 }
 
