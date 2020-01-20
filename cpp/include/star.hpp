355c0ce506ac3d717e854df306a81ed1d2fb952a
Konstantin Malanchev <hombit@gmail.com>
2020-01-20 16:50:07 +0300

    Introduce IrrSource

--- cpp/include/star.hpp
+++ cpp/include/star.hpp
@@ -2,6 +2,7 @@
 #define FREDDI_STAR_HPP
 
 #include <functional> // function
+#include <memory> // unique_ptr
 #include <optional>
 #include <vector>
 
@@ -13,7 +14,7 @@
 class Star {
 protected:
 	struct IrradiatedProperties {
-		std::optional<vald> Tirr;
+		std::optional<vald> Qirr;
 		std::optional<vald> Teff;
 		std::optional<double> luminosity;
 	};
@@ -22,28 +23,74 @@ protected:
 	const vald Tth_;
 private:
 	static std::vector<Triangle> initializeTriangles(double radius, unsigned short grid_scale);
+protected:
 	IrradiatedProperties irr_;
 protected:
 	void invalidate_irradiated_properties();
 public:
 	Star(double temp, double radius, unsigned short lod);
 protected:
-	double integrate(std::function<double (size_t)> func) const;
-	double integrate(std::function<double (size_t)> func, const UnitVec3& direction) const;
+	double integrate(std::function<double (size_t)>&& func) const;
+	double integrate(std::function<double (size_t)>&& func, const UnitVec3& direction) const;
 public:
 	const std::vector<Triangle>& triangles() const;
 	const vald& Tth() const;
-	virtual const vald& Tirr();
+	virtual const vald& Qirr();
 	const vald& Teff();
 	double luminosity();
 };
 
 
-class FreddiStar: public Star {
+class IrrSource {
+protected:
+	Vec3 position_;
+	double luminosity_;
+public:
+	IrrSource(const Vec3& position);
+	virtual double irr_flux(const Vec3& coord, const UnitVec3& normal) const;
+	const Vec3& position() const;
+	double luminosity() const;
+	void set_luminosity(double value);
+	static double cos_object(const UnitVec3& unit_distance, const UnitVec3& normal);
+};
+
+
+class PointSource: public IrrSource {
+public:
+	using IrrSource::IrrSource;
+public:
+	double irr_flux(const Vec3& coord, const UnitVec3& normal) const override;
+};
+
+
+class PlainSource: public IrrSource {
+protected:
+	UnitVec3 plain_normal_;
+public:
+	PlainSource(const Vec3& position, const UnitVec3& plain_normal);
+public:
+	double irr_flux(const Vec3& coord, const UnitVec3& normal) const override;
+	const UnitVec3& plain_normal() const;
+};
+
+
+class IrradiatedStar: public Star {
+protected:
+	std::vector<std::unique_ptr<IrrSource>> sources_;
+public:
+	IrradiatedStar(std::vector<std::unique_ptr<IrrSource>>&& sources,
+			double temp, double radius, unsigned short lod);
+public:
+	const std::vector<std::unique_ptr<IrrSource>>& sources() const;
+	const vald& Qirr() override;
+};
+
+
+class FreddiStar: public IrradiatedStar {
 protected:
 	FreddiArguments args;
 public:
-	FreddiStar(const FreddiArguments& args);
+	FreddiStar(const FreddiArguments& args, std::vector<std::unique_ptr<IrrSource>>&& sources);
 };
 
 
--- cpp/src/star.cpp
+++ cpp/src/star.cpp
@@ -2,6 +2,7 @@
 
 #include <constants.hpp>
 #include <star.hpp>
+#include <util.hpp>
 
 
 Star::Star(const double temp, const double radius, const unsigned short lod):
@@ -29,7 +30,7 @@ const vald& Star::Tth() const {
 	return Tth_;
 }
 
-double Star::integrate(std::function<double (size_t)> func) const {
+double Star::integrate(std::function<double (size_t)>&& func) const {
 	double sum = 0;
 	for (size_t i = 0; i < triangles().size(); ++i) {
 		sum += triangles()[i].area() * func(i);
@@ -37,7 +38,7 @@ double Star::integrate(std::function<double (size_t)> func) const {
 	return sum;
 }
 
-double Star::integrate(std::function<double (size_t)> func, const UnitVec3& direction) const {
+double Star::integrate(std::function<double (size_t)>&& func, const UnitVec3& direction) const {
 	double sum = 0;
 	for (size_t i = 0; i < triangles().size(); ++i) {
 		sum += triangles()[i].area_cos(direction) * func(i);
@@ -45,16 +46,16 @@ double Star::integrate(std::function<double (size_t)> func, const UnitVec3& dire
 	return sum;
 }
 
-const vald& Star::Tirr() {
-	if (!irr_.Tirr) {
-		irr_.Tirr = vald(0.0, triangles().size());
+const vald& Star::Qirr() {
+	if (!irr_.Qirr) {
+		irr_.Qirr = vald(0.0, triangles().size());
 	}
-	return *irr_.Tirr;
+	return *irr_.Qirr;
 }
 
 const vald& Star::Teff() {
 	if (!irr_.Teff) {
-		irr_.Teff = std::pow(m::pow<4>(Tth()) + m::pow<4>(Tirr()), 0.25);
+		irr_.Teff = std::pow(Qirr() / GSL_CONST_CGSM_STEFAN_BOLTZMANN_CONSTANT + m::pow<4>(Tth()), 0.25);
 	}
 	return *irr_.Teff;
 }
@@ -69,6 +70,80 @@ double Star::luminosity() {
 }
 
 
-FreddiStar::FreddiStar(const FreddiArguments& args):
-		Star(args.basic->Topt, args.basic->Ropt, args.calc->starlod),
+IrrSource::IrrSource(const Vec3& position):
+		position_(position),
+		luminosity_(0.0) {}
+
+double IrrSource::irr_flux(const Vec3& coord, const UnitVec3& normal) const {
+	return 0.0;
+}
+
+const Vec3& IrrSource::position() const {
+	return position_;
+}
+
+double IrrSource::luminosity() const {
+	return luminosity_;
+}
+
+void IrrSource::set_luminosity(double value) {
+	luminosity_ = value;
+}
+
+double IrrSource::cos_object(const UnitVec3& unit_distance, const UnitVec3& normal) {
+	double cos = unit_distance.dotProduct(normal);
+	if (cos <= 0.0) {
+		return 0.0;
+	}
+	return cos;
+}
+
+
+double PointSource::irr_flux(const Vec3& coord, const UnitVec3& normal) const {
+	const auto vec = coord - position();
+	return luminosity() / (FOUR_M_PI * m::pow<2>(vec.r())) * cos_object(UnitVec3(vec), normal);
+}
+
+
+PlainSource::PlainSource(const Vec3& position, const UnitVec3& plain_normal):
+		IrrSource(position),
+		plain_normal_(plain_normal) {}
+
+double PlainSource::irr_flux(const Vec3& coord, const UnitVec3& normal) const {
+	const auto vec = coord - position();
+	const auto uvec = UnitVec3(vec);
+	const double cos_source = plain_normal().dotProduct(uvec);
+	return luminosity() * 2.0 * cos_source / m::pow<2>(vec.r()) * cos_object(uvec, normal);
+}
+
+const UnitVec3& PlainSource::plain_normal() const {
+	return plain_normal_;
+}
+
+
+IrradiatedStar::IrradiatedStar(std::vector<std::unique_ptr<IrrSource>>&& sources,
+		const double temp, const double radius, const unsigned short lod):
+		Star(temp, radius, lod),
+		sources_(std::move(sources)) {}
+
+const std::vector<std::unique_ptr<IrrSource>>& IrradiatedStar::sources() const {
+	return sources_;
+}
+
+const vald& IrradiatedStar::Qirr() {
+	if (!irr_.Qirr) {
+		vald x(0.0, triangles().size());
+		for (size_t i = 0; i < triangles().size(); ++i) {
+			for (const auto& source : sources()) {
+				x[i] += source->irr_flux(triangles_[i].center(), triangles_[i].normal());
+			}
+		}
+		irr_.Qirr = std::move(x);
+	}
+	return *irr_.Qirr;
+}
+
+
+FreddiStar::FreddiStar(const FreddiArguments& args, std::vector<std::unique_ptr<IrrSource>>&& sources):
+		IrradiatedStar(std::move(sources), args.basic->Topt, args.basic->Ropt, args.calc->starlod),
 		args(args) {}
