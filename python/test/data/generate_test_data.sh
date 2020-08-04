1d140ed5d39e067d5b23ecd4b5652227e6c2a937
Konstantin Malanchev <hombit@gmail.com>
2020-08-04 16:12:10 +0300

    Now quasistat_Thot_Cirr_Tirr.dat makes sense

--- python/test/data/generate_test_data.sh
+++ python/test/data/generate_test_data.sh
@@ -12,6 +12,6 @@ $FREDDI --prefix=gaussF_Thot --Thot=1e4 --initialcond=gaussF
 
 $FREDDI --prefix=quasistat_Mdisk0 --initialcond=quasistat --Mdisk0=1e26
 
-$FREDDI --prefix=quasistat_Thot_Cirr_Tirr --Cirr=1e-3 --angulardistdisk=isotropic --boundcond=Tirr --initialcond=quasistat
+$FREDDI --prefix=quasistat_Thot_Cirr_Tirr --Cirr=2e-4 --angulardistdisk=isotropic --boundcond=Tirr --Thot=1e4 --initialcond=quasistat
 
 $FREDDI --prefix=lambdas --lambda=8000 --lambda=5000 --lambda=3000
