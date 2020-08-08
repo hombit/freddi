5e8a214a175cab1cbf9f1742195ee01f06f57f32
Galina Lipunova <galja@jet.asc.rssi.ru>
2020-08-08 09:52:50 +0300

    Corrected Sigma_plus

--- cpp/src/freddi_state.cpp
+++ cpp/src/freddi_state.cpp
@@ -660,12 +660,12 @@ double FreddiState::Sigma_minus(double r) const {
 	return 74.6 * std::pow(args().basic->alphacold / 0.1, -0.83) * std::pow(r / 1e10, 1.18)
 		* std::pow(args().basic->Mx / GSL_CONST_CGSM_SOLAR_MASS, -0.40);
 }
+
 double FreddiState::Sigma_plus(double r) const {
 	// Lasota et al., A&A 486, 523–528 (2008), Eq A.1, DOI: 10.1051/0004-6361:200809658
-	return 74.6 * std::pow(args().basic->alpha / 0.1, -0.83) * std::pow(r / 1e10, 1.18)
-		* std::pow(args().basic->Mx / GSL_CONST_CGSM_SOLAR_MASS, -0.40);
+	return 39.9 * std::pow(args().basic->alpha / 0.1, -0.80) * std::pow(r / 1e10, 1.11)
+		* std::pow(args().basic->Mx / GSL_CONST_CGSM_SOLAR_MASS, -0.37);
 }
-
 
 double FreddiState::v_cooling_front(double r) {
         // The cooling-front velocity depends on the ratio between the current Sigma and critical Sigmas
