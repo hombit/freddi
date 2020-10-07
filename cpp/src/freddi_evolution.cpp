67f6cc74731bbd9697a3ef8346d9c03ccdc79368
ScorpionesXone <artur_ava97@mail.ru>
2020-10-07 18:35:07 +0300

    Tirr is always less than Thot

--- cpp/src/freddi_evolution.cpp
+++ cpp/src/freddi_evolution.cpp
@@ -44,11 +44,12 @@ void FreddiEvolution::truncateOuterRadius() {
 	auto ii = last() + 1;
 	if (Tirr().at(last()) / Tph_vis().at(last()) < args().disk->Tirr2Tvishot) {
 	// when irradiation is not important
-	// hot disc extends as far as Sigma>Sigma_max_cold(alpha_cold) and not farther than R_cooling_front
+	// hot disc extends as far as Sigma>Sigma_max_cold(alpha_cold) and not farther than R_cooling_front and Tirr <= Thot 
 		do {
 			ii--;
 			if (ii <= first()) throw std::runtime_error("Rout <= Rin");
-		} while( ( R().at(ii) > R_cooling_front ( R().at(ii)) ) && ( Sigma().at(ii) < Sigma_minus(R().at(ii)) ) );
+		  } while( ( R().at(ii) > R_cooling_front ( R().at(ii)) ) && ( Sigma().at(ii) < Sigma_minus(R().at(ii)) ) && ( Tirr().at(ii) < args().disk->Thot ) );
+		//} while( ( R().at(ii) > R_cooling_front ( R().at(ii)) ) && ( Sigma().at(ii) < Sigma_minus(R().at(ii)) ) );
 	} else if (args().disk->boundcond == "Teff") {
 	// irradiation is important, the boundary is at fixed Teff
 		do {
