1e1bc0815487c3b1492f7d6fa4945dcab3af204b
Konstantin Malanchev <hombit@gmail.com>
2024-05-28 09:19:09 -0400

    Regression test for issue#66

--- python/test/data/generate_test_data.sh
+++ python/test/data/generate_test_data.sh
@@ -21,3 +21,6 @@ $FREDDI --prefix=lambdas --lambda=8000 --lambda=5000 --lambda=3000
 # Passbands files are from
 # http://svo2.cab.inta-csic.es/svo/theory/fps3/index.php?mode=browse&gname=Swift&asttype=
 $FREDDI --prefix=passbands --time=10 --tau=1 --passband=passbands/Swift_B.dat --passband=passbands/Swift_V.dat
+
+# https://github.com/hombit/freddi/issues/66
+$FREDDI --prefix=issue66 --Mx=9.4 --period=1.116 --Mopt=2.5 --alpha=0.23 --colourfactor=1.7 --inclination=20.7 --time=35 --tau=1 --distance=9.1 --Mdisk0=2.70002635e+25 --Cirr=1.5e-4 --kerr=0.4 --rout=0.7 --opacity=Kramers --initialcond=powerF --powerorder=1.4
