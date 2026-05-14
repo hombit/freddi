df21899ad3326b92a169ddcaf9421beddff1c92d
gvlipunova <gvlipunova@gmail.com>
2026-05-14 17:30:53 +0200

    Added Sigma_wind in vertical direction to output

--- cpp/src/output.cpp
+++ cpp/src/output.cpp
@@ -224,7 +224,7 @@ std::vector<FileOutputShortField> FreddiFileOutput::initializeShortFields(const 
 			{"Mdot_Rhot", "g/s", "Accretion rate at the cooling front radius", [freddi]() {return freddi->Mdot_out_from_F();}},
 			{"Mdot_wind", "g/s", "Wind mass loss from two sides",  [freddi]() {return freddi->Mdot_wind();}},
 			{"R_IC", "cm", "Compton radius", [freddi]() {return freddi->R_IC();}},
-			
+			{"Sigma_wind", "g/cm^2", "Surface density in corona/wind at the outer radius of the hot disk", [freddi]() {return freddi->Column_density_wind()[freddi->last()];}},
 	};
 	const bool cold_disk = freddi->args().flux->cold_disk;
 	const bool star = freddi->args().flux->star;
